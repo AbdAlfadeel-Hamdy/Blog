3677d0980b865a862dc1ed78339cab87c6a499a6
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-08-13 20:03:17 +0200

    Redux thunk

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { legacy_createStore } from "redux";
+import { legacy_createStore, applyMiddleware } from "redux";
 import App from "./components/App";
-import Reducers from "./reducers";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
+const store = legacy_createStore(reducers, applyMiddleware(thunk));
 root.render(
-  <Provider store={legacy_createStore(Reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>
 );
