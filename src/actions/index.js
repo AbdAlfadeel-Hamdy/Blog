943856dc991fe7a6b3f2a5ae4a54c8a20af5467e
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-08-15 03:34:14 +0200

    Dispatching

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 import _ from "lodash";
+export const fetchPostsAndUsers = () => async (dispatch) => {
+  await dispatch(fetchPosts());
+};
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
   dispatch({
@@ -8,11 +11,19 @@ export const fetchPosts = () => async (dispatch) => {
   });
 };
 
-export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({
     type: "FETCH_USERS",
     payload: response.data,
   });
-});
+};
+
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({
+//     type: "FETCH_USERS",
+//     payload: response.data,
+//   });
+// });
